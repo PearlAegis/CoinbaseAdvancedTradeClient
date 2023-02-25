53509015111eb3dc8a3f52b9254c438fd965942a
Mike Pearl <pearlaegis@outlook.com>
2023-02-25 15:58:24 -0700

    Remove unused RequestHeader constants

--- CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient/Constants/RequestHeaders.cs
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient/Constants/RequestHeaders.cs
@@ -5,8 +5,5 @@
         public const string AccessKey = "CB-ACCESS-KEY";
         public const string AccessSign = "CB-ACCESS-SIGN";
         public const string AccessTimestamp = "CB-ACCESS-TIMESTAMP";
-        public const string After = "cb-after";
-        public const string Before = "cb-before";
-        public const string MediaTypeApplicationJson = "application/json";
     }
 }
