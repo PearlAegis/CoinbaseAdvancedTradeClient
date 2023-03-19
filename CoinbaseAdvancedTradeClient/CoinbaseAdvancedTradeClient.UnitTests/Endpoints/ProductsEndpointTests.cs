2729058bae514ef9920de9dc84d4d7d90d15efde
Mike Pearl <pearlaegis@outlook.com>
2023-03-18 22:27:18 -0700

    Begin adding product endpoint unit tests

---
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient.UnitTests/Endpoints/ProductsEndpointTests.cs
@@ -0,0 +1,153 @@
+﻿using CoinbaseAdvancedTradeClient.Interfaces;
+using CoinbaseAdvancedTradeClient.Models.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoinbaseAdvancedTradeClient.UnitTests.Endpoints
+{
+    public class ProductsEndpointTests
+    {
+        private readonly ICoinbaseAdvancedTradeApiClient _testClient;
+
+        public ProductsEndpointTests()
+        {
+            var config = new ApiClientConfig()
+            {
+                ApiKey = "key",
+                ApiSecret = "secret"
+            };
+
+            _testClient = new CoinbaseAdvancedTradeApiClient(config);
+        }
+
+        #region GetListProductsAsync
+
+        [Fact]
+        public async Task GetListProductsAsync_ValidRequestAndResponseJson_ReturnsSuccessfulApiResponse()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public async Task GetListProductsAsync_ValidRequestAndResponseJson_ResponseHasValidProductsPage()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public async Task GetListProductsAsync_ValidRequestAndResponseJson_ResponseHasValidProducts()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public async Task GetListProductsAsync_InvalidResponseJson_ReturnsUnsuccessfulApiResponse()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Theory]
+        [InlineData(251)]
+        [InlineData(0)]
+        [InlineData(-25)]
+        public async Task GetListProductsAsync_InvalidLimitRange_ReturnsUnsuccessfulApiResponse(int limit)
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Theory]
+        [InlineData(251)]
+        [InlineData(0)]
+        [InlineData(-25)]
+        public async Task GetListProductsAsync_InvalidOffsetRange_ReturnsUnsuccessfulApiResponse(int offset)
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public async Task GetListProductsAsync_InvalidProductType_ReturnsUnsuccessfulApiResponse()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+
+        [Fact]
+        public async Task GetListProductsAsync_UnauthorizedResponseStatus_ReturnsUnsuccessfulApiResponse()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+
+        #endregion // GetListProductsAsync
+
+        #region GetProductAsync
+
+
+
+        #endregion // GetProductAsync
+
+        #region GetProductCandlesAsync
+
+
+
+        #endregion // GetProductcandlesAsync
+
+        #region GetMarketTradesAsync
+
+
+
+        #endregion // GetMarketTradesAsync
+    }
+}
