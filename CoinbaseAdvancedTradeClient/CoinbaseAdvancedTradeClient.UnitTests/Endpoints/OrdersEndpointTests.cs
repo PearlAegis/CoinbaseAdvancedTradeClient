19e093b802f36e1da768c844284bbbf0ed423be9
Mike Pearl <pearlaegis@outlook.com>
2023-03-27 21:39:46 -0700

    Add OrdersEndpointTests file

---
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient.UnitTests/Endpoints/OrdersEndpointTests.cs
@@ -0,0 +1,59 @@
+﻿using CoinbaseAdvancedTradeClient.Interfaces;
+using CoinbaseAdvancedTradeClient.Models.Config;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace CoinbaseAdvancedTradeClient.UnitTests.Endpoints
+{
+    public class OrdersEndpointTests
+    {
+        private readonly ICoinbaseAdvancedTradeApiClient _testClient;
+
+        public OrdersEndpointTests()
+        {
+            var config = new ApiClientConfig()
+            {
+                ApiKey = "key",
+                ApiSecret = "secret"
+            };
+
+            _testClient = new CoinbaseAdvancedTradeApiClient(config);
+        }
+
+        #region GetListOrdersAsync
+
+        [Fact]
+        public async Task GetListOrdersAsync_ValidRequestAndResponseJson_ReturnsSuccessfulApiResponse()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetListOrdersAsync_ValidRequestAndResponseJson_ResultHasValidOrdersPage()
+        {
+
+        }
+
+        [Fact]
+        public async Task GetListOrdersAsync_ValidRequestAndResponseJson_ResponseHasValidOrders()
+        {
+
+        }
+
+        #endregion // GetListOrdersAsync
+
+        #region GetListFillsAsync
+
+
+        #endregion // GetListFillsAsync
+
+        #region GetOrderAsync
+
+
+        #endregion // GetOrderAsync
+    }
+}
