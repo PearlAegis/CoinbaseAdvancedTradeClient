40459fe10e4c914d9b535dd0e96a9976cfe74ee0
Mike Pearl <pearlaegis@outlook.com>
2023-05-03 17:41:11 -0700

    Add EnumExtension method to get value from enum member

---
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Runtime.Serialization;
+
+namespace CoinbaseAdvancedTradeClient.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string? GetEnumMemberValue(this Enum member)
+        {
+            if (member != null)
+            {
+                var type = member.GetType();
+                var info = type.GetMember(member.ToString());
+                var attributes = info[0].GetCustomAttributes(typeof(EnumMemberAttribute), false);
+                if (attributes.Length > 0)
+                {
+                    var enumMember = (EnumMemberAttribute)attributes[0];
+                    return enumMember.Value;
+                }
+            }
+
+            return null;
+        }
+    }
+}
