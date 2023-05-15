b7aac4eafbc715c34649f3ce2e40b4d407ec08bd
Mike Pearl <pearlaegis@outlook.com>
2023-05-14 21:03:04 -0700

    Replace constants with enums

---
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient/Enums/CandleGranularity.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.Serialization;
+
+namespace CoinbaseAdvancedTradeClient.Enums
+{
+    public enum CandleGranularity
+    {
+        [EnumMember(Value = "ONE_MINUTE")]
+        OneMinute,
+
+        [EnumMember(Value = "FIVE_MINUTE")]
+        FiveMinute,
+        
+        [EnumMember(Value = "FIFTEEN_MINUTE")]
+        FifteenMinute,
+        
+        [EnumMember(Value = "THIRTY_MINUTE")]
+        ThirtyMinute,
+
+        [EnumMember(Value = "ONE_HOUR")]
+        OneHour,
+
+        [EnumMember(Value = "TWO_HOUR")]
+        TwoHour,
+
+        [EnumMember(Value = "SIX_HOUR")]
+        SixHour,
+
+        [EnumMember(Value = "ONE_DAY")]
+        OneDay
+    }
+}
---
+++ CoinbaseAdvancedTradeClient/CoinbaseAdvancedTradeClient/Enums/OrderType.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Runtime.Serialization;
+
+namespace CoinbaseAdvancedTradeClient.Enums
+{
+    public enum OrderType
+    {
+        [EnumMember(Value = "MARKET")]
+        Market,
+
+        [EnumMember(Value = "LIMIT")]
+        Limit,
+
+        [EnumMember(Value = "STOP")]
+        Stop,
+
+        [EnumMember(Value = "STOP_LIMIT")]
+        StopLimit
+    }
+}
